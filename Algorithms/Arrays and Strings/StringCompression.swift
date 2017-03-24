1dac4bb997d347fcb81e74d6068fbab730f79400
Rahul Ranjan <rahul.ranjan@udacity.com>
2017-03-25 00:46:13 +0530

    create separate folder for each group

---
+++ Algorithms/Arrays and Strings/StringCompression.swift
@@ -0,0 +1,14 @@
+//
+//  StringCompression.swift
+//  Algorithms
+//
+//  Created by Rahul Ranjan on 3/25/17.
+//  Copyright © 2017 Rahul Ranjan. All rights reserved.
+//
+
+import Foundation
+
+
+func stringCompression(input: String) {
+    
+}
