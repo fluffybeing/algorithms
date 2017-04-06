36bcfb933ad48ceca4898d0b4511e83bff73403f
Rahul Ranjan <rahul.ranjan@udacity.com>
2017-04-06 16:40:02 +0530

    complete priority queue implementation using heap

---
+++ Algorithms/Tree and Graph/Algos/Prims.swift
@@ -0,0 +1,17 @@
+//
+//  Prims.swift
+//  Algorithms
+//
+//  Created by Rahul Ranjan on 4/6/17.
+//  Copyright © 2017 Rahul Ranjan. All rights reserved.
+//
+
+import Foundation
+
+struct Prims {
+    var msSet = [Int]()
+    
+    init() {
+        
+    }
+}
---
+++ Algorithms/Tree and Graph/PriorityQueue.swift
@@ -0,0 +1,88 @@
+//
+//  File.swift
+//  Algorithms
+//
+//  Created by Rahul Ranjan on 4/6/17.
+//  Copyright © 2017 Rahul Ranjan. All rights reserved.
+//
+
+import Foundation
+
+
+// Priority Queue Implementaton
+struct PriorityQueue<T:Comparable> {
+    
+    var container = [T]()
+    
+    // check if queue is empty
+    public func isEmpty() -> Bool {
+        return container.isEmpty
+    }
+    
+    public mutating func insert(element: T) {
+        container.append(element)
+        
+        heapify()
+    }
+    
+
+    // highest priority
+    public func highestProrityElement() -> T {
+        return container.first!
+    }
+    
+    // Get the min element
+    public mutating func extract() -> T {
+        let element = container.removeFirst()
+        
+        // order the element
+        heapify()
+        return element
+    }
+    
+    // Build the min heap
+    private mutating func heapify() {
+        for index in stride(from: (container.count - 1) / 2, to: -1, by: -1) {
+            min_heapify(index: index)
+        }
+    }
+    
+    // we will use heap for the priority queue
+    // use array as a tree
+    private mutating func min_heapify(index: Int) {
+        let left = 2 * index + 1
+        let right = 2 * index + 2
+        
+        var smallest = index
+
+        
+        // check with left subtree node
+        if (left < container.count && container[left] < container[index] ) {
+            smallest = left
+        }
+        
+        // check with the right subtree node
+        if (right < container.count && container[right] < container[smallest]) {
+            smallest = right
+        }
+        
+        // Now check if smallest number is equal to root
+        if smallest != index {
+            swap(&container[index], &container[smallest])
+            min_heapify(index: smallest)
+        }
+        
+    }
+}
+
+
+func priorityQueueTest() {
+    var pq = PriorityQueue<Int>()
+    
+    pq.insert(element: 10)
+    pq.insert(element: 3)
+    pq.insert(element: 2)
+    pq.insert(element: 1)
+    
+    assert(pq.highestProrityElement() == 1)
+}
