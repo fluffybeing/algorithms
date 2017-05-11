8c5e2dbf4724da508513ab8651d755d6b37f7d34
Rahul Ranjan <rahul.rrixe@gmail.com>
2017-05-11 12:25:17 +0530

    improve the fizzbuzz

--- Algorithms/LetsBuildThatApp/FizzBuzz.swift
+++ Algorithms/LetsBuildThatApp/FizzBuzz.swift
@@ -11,17 +11,18 @@ import Foundation
 
 func fizzbuzz(numbers: Int) {
     for number in 0..<numbers {
+        let threeDivisor = number % 3
+        let fiveDivisor = number % 5
         
-        if number % 3 == 0 && number % 5 == 0 {
+        if threeDivisor == 0 && fiveDivisor == 0 {
             print("FizzBuzz")
-        } else if number % 3 == 0 {
+        } else if threeDivisor == 0 {
             print("fizz")
-        } else if number % 5 == 0 {
+        } else if fiveDivisor == 0 {
             print("buzz")
         }
     }
 }
-
 
 func fizzbuzzTest() {
     fizzbuzz(numbers: 10)
