78f76f101aeb5ba48fb97e1974f90841162bf2ca
Rahul Ranjan <rahul.rrixe@gmail.com>
2017-05-23 23:26:45 +0530

    complete question 496

---
+++ Algorithms/LeetCode/NextGreaterElement.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+func nextGreaterElement(_ findNums: [Int], _ nums: [Int]) -> [Int] {
+	
+	var findNums = findNums
+	let numsCount = nums.count
+	
+	for (index, value) in findNums.enumerated() {
+		if let i = nums.index(of: value) {
+			let rightElementIndex = i + 1 
+			
+			if rightElementIndex < numsCount {
+				let slice = Array(nums[rightElementIndex..<numsCount])
+				if let elementIndex = slice.index(where: {$0 > value}) {
+					findNums[index] = slice[elementIndex]
+				} else {
+					findNums[index] = -1
+				}
+			} else {
+				findNums[index] = -1
+			} 
+		}	
+	}	
+	return findNums
+}
+
+let result = nextGreaterElement([4,1,2], [1,3,4,2])
+// [7,7,7,7,7]
+print(result)
