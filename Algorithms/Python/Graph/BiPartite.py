a62ca393e1c2c601c353db8c7ec69bd63e3599c7
Rahul Ranjan <rahul.rrixe@gmail.com>
2017-06-12 22:03:17 +0530

    complete check bipartite

---
+++ Algorithms/Python/Graph/BiPartite.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+
+def dfs(s, g, v, b):
+	if v[s] == 1:
+		return
+	v[s] = 1
+
+	for n in g[s]:
+		if b[s] == b[n]:
+			return True
+	
+		if v[n] == 0 and b[n] == None:
+			b[n] = 'r' if b[s] == 'b' else 'b'
+			return dfs(n, g, v, b)
+
+
+def isBipartite(g):
+	v = {k:0 for k in g.keys()}
+	b = {k:None for k in g.keys()}
+	b[1] = 'b'
+	if dfs(1, g, v, b):
+		return True
+	return False
+
+
+
+def testisBipartite():
+	g = {
+		1: [2, 4],
+		2: [1, 3],
+		4: [1],
+		3: [2, 5],
+		5: [3]
+	}
+	print(isBipartite(g))
+	
+testisBipartite()
