402a1ffd9cdf8017828d7ead8fa837a591ca27e2
Rahul Ranjan <rahul.rrixe@gmail.com>
2019-03-05 14:29:34 +0800

    complete myatoi challenge

--- Algorithms/Python/LeetCode/MyAtoi.py
+++ Algorithms/Python/LeetCode/MyAtoi.py
@@ -1,11 +1,30 @@
 #!/usr/bin/python3
 
+# 8. String to Integer (atoi)
+
+def intOverflow(num: int) -> int:
+    INT_MIN = -2147483648
+    INT_MAX = 2147483647
+
+    if num < INT_MIN:
+        return INT_MIN
+    elif num > INT_MAX:
+        return INT_MAX
+    else:
+        return num
+
+def isOnlyNegation(num: str) -> bool:
+    if num == "-" or num == "+":
+        return True
+    return False
+
 def myAtoi(str: str) -> int:
     num = ""
+
     for (index, char) in enumerate(str):
-        if type(int(char)) is int:
+        if char.isdigit():
             num += char
-        elif char == "-":
+        elif char == "-" or char == "+":
             if index == 0:
                 num += char
             else:
@@ -14,15 +33,23 @@ def myAtoi(str: str) -> int:
                     num += char
                 else:
                     break
-        elif if char == " ":
-            if index + 1 < len(str):
-                afterChar = str[index + 1]
-                if type(afterChar) is not int:
+        elif char == " ":
+            if index > 0:
+                beforeChar = str[index - 1]
+                if beforeChar == " ":
+                    continue
+                else: 
                     break
         else:
             break
-    
+
     if num:
-        return int(num)
+        num = int(num) if not isOnlyNegation(num) else 0
+        return intOverflow(num)
     else:
         return 0
+
+def main():
+    print(myAtoi(" + 1"))
+
+main()
