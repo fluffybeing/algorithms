eff02047aa5c2772e5a6826eff57df240c879530
Rahul Ranjan <rahul.rrixe@gmail.com>
2019-05-12 23:15:34 +0800

    finish the leetcode problem

---
+++ Algorithms/LeetCode/Longest Substring.swift
@@ -0,0 +1,23 @@
+// Input: "abcabcbb"
+// Output: 3 
+// Explanation: The answer is "abc", with the length of 3. 
+
+func lengthOfLongestSubstring(_ s: String) -> Int {
+    guard !s.isEmpty else { return 0 }
+
+    var length = 0
+    var substring = [Character]()
+
+    for c in s {
+        if substring.contains(c), let index = substring.index(of: c) {
+            length = max(length, substring.count)
+            substring.removeSubrange(0...index)
+        }
+        substring.append(c)
+    }
+
+    return max(length, substring.count)
+}
+
+let result = lengthOfLongestSubstring("dvdf")
+print(result)
