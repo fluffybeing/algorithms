fbc9b991924da5bd0da13667639827e4a8b7ca62
Rahul Ranjan <rahul.rrixe@gmail.com>
2019-06-07 22:34:18 +0800

    Add solution for the reverse integer

---
+++ Algorithms/LeetCode/ReverseInt.swift
@@ -0,0 +1,23 @@
+// import Foundation
+
+class Solution {
+    func reverse(_ x: Int) -> Int {
+        var reverseInt = 0
+        var x = x
+        
+        while(abs(x) > 0) {
+            reverseInt *= 10
+            reverseInt += (x % 10) 
+            x = Int(x / 10)
+        } 
+        
+        guard reverseInt > (-2 << 31) && reverseInt < (2 << 31) - 1 else {
+            return 0
+        }
+
+        return reverseInt
+    }
+}
+
+// let s = Solution()
+// print(s.reverse(123))
