97731407c272620950503d2304f82121ed7a3631
Rahul Ranjan <rahul.rrixe@gmail.com>
2019-06-07 19:43:36 +0800

    Update the indendation

--- Algorithms/LeetCode/Array Partition.swift
+++ Algorithms/LeetCode/Array Partition.swift
@@ -4,15 +4,15 @@ import Foundation
 // Question 561
 
 func arrayPairSum(_ nums: [Int]) -> Int {
-	
-	let sortedNums = nums.sorted()
-	
-	var sum = 0
-	for index in stride(from: 0, to: sortedNums.count, by: 2) {
-		sum += min(sortedNums[index], sortedNums[index+1])
-	}
-	
-	return sum
+
+  let sortedNums = nums.sorted()
+
+  var sum = 0
+  for index in stride(from: 0, to: sortedNums.count, by: 2) {
+          sum += min(sortedNums[index], sortedNums[index+1])
+  }
+
+  return sum
 }
 
 let result = arrayPairSum([1, 4, 2, 3])
