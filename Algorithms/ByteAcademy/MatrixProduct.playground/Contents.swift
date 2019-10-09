87a2b130e369a7580cf5336e22ef06eec136ea64
Rahul Ranjan <rahul.rrixe@gmail.com>
2019-10-09 14:05:15 +0800

    update playgrounds for new code

---
+++ Algorithms/ByteAcademy/MatrixProduct.playground/Contents.swift
@@ -0,0 +1,24 @@
+import Cocoa
+
+func matrixProduct(_ mat: [[Int]]) -> Int {
+  return 0
+}
+
+func matrixProductUtil(_ mat: [[Int]], visit: inout [[Int]],
+                       x: Int, y: Int, product: inout Int) {
+  guard !mat.isEmpty else { return }
+
+  if x == mat.count && y == mat[0].count {
+    return
+  }
+
+  let down = mat[x + 1][y]
+  product = max(product, product * down)
+  visit[x + 1][y] = 1
+  matrixProductUtil(mat, visit: &visit, x: x + 1, y: y, product: &product)
+
+  let right = mat[x][y + 1]
+  product = max(product, product * right)
+  visit[x][y + 1] = 1
+  matrixProductUtil(mat, visit: &visit, x: x, y: y + 1, product: &product)
+}
