8f67971591c46d62e0c5008b2dfb1d0c3cfd5bfa
Rahul Ranjan <rahul.rrixe@gmail.com>
2019-10-28 16:29:55 +0800

    Finish the matrix product problem

--- Algorithms/ByteAcademy/MatrixProduct.playground/Contents.swift
+++ Algorithms/ByteAcademy/MatrixProduct.playground/Contents.swift
@@ -1,28 +1,84 @@
 import Cocoa
 
 func matrixProduct(_ mat: [[Int]]) -> Int {
-  return 0
-}
+  guard !mat.isEmpty else { return 0 }
 
-func matrixProductUtil(_ mat: [[Int]], visit: inout [[Int]],
-                       x: Int, y: Int, product: inout Int) {
-  guard !mat.isEmpty else { return }
+  let rowSize = mat.count
+  let columnSize = mat[0].count
 
-  if x == mat.count && y == mat[0].count {
-    return
+  var maxCache = Array(repeating: Array(repeating: 0, count: columnSize), count: rowSize)
+  var minCache = Array(repeating: Array(repeating: 0, count: columnSize), count: rowSize)
+
+  for i in 0..<rowSize {
+    for j in 0..<columnSize {
+      var minVal = Int.max
+      var maxVal = Int.min
+
+      if i == 0 && j == 0 {
+        maxCache[i][j] = mat[i][j]
+        minCache[i][j] = mat[i][j]
+        continue
+      }
+
+
+      if i > 0 {
+        let tempMax = max(mat[i][j] * maxCache[i - 1][j], mat[i][j] * minCache[i - 1][j])
+        maxVal = max(maxVal, tempMax)
+        let tempMin = min(mat[i][j] * maxCache[i - 1][j], mat[i][j] * minCache[i - 1][j])
+        minVal = min(minVal, tempMin)
+      }
+
+      if j > 0 {
+        let tempMax = max(mat[i][j] * maxCache[i][j - 1], mat[i][j] * minCache[i][j - 1])
+        maxVal = max(maxVal, tempMax)
+        let tempMin = min(mat[i][j] * maxCache[i][j - 1], mat[i][j] * minCache[i][j - 1])
+        minVal = min(minVal, tempMin)
+      }
+
+      maxCache[i][j] = maxVal
+      minCache[i][j] = minVal
+
+      print(maxCache)
+      print(minCache)
+      print("--------")
+    }
   }
 
-  let down = mat[x + 1][y]
-  product = max(product, product * down)
-  visit[x + 1][y] = 1
-  matrixProductUtil(mat, visit: &visit, x: x + 1, y: y, product: &product)
+  return maxCache[rowSize - 1][columnSize - 1]
+}
 
-  let right = mat[x][y + 1]
-  product = max(product, product * right)
-  visit[x][y + 1] = 1
-  matrixProductUtil(mat, visit: &visit, x: x, y: y + 1, product: &product)
+// Be careful about the boundary conditions
+func matrixProductPositive(_ mat: [[Int]]) -> Int {
+  guard !mat.isEmpty else {
+    return 0
+  }
+
+  var row = 0
+  var column = 0
+  var maxProduct = mat[row][column]
+
+  while row < mat.count-1 || column < mat[0].count-1 {
+    var down = Int.min
+    var right = Int.min
+
+    if row < mat.count-1 {
+      down = maxProduct * mat[row + 1][column]
+    }
+
+    if column < mat[0].count-1 {
+      right = maxProduct * mat[row][column + 1]
+    }
+
+    right >= down ? (column += 1) : (row += 1)
+    maxProduct = max(right, down)
+  }
+
+  return maxProduct
 }
 
 
-
+let m = [[-1, 2, 3], [4, 5, -6], [7, 8, 9]]
+//let result = matrixProductPositive(m)
+let result = matrixProduct(m)
+print(result)
 
