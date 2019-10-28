ccb6dfeea71fbdac962a4ae9105c004544c10e7d
Rahul Ranjan <rahul.rrixe@gmail.com>
2019-10-28 16:45:52 +0800

    Finish the code for finding duplicates

---
+++ Algorithms/ByteAcademy/FindDuplicates.playground/Contents.swift
@@ -0,0 +1,24 @@
+import Cocoa
+
+func findDuplicates(a: [Int]) -> [Int] {
+  guard !a.isEmpty else { return [] }
+
+  var hashSet: [Int: Int] {
+    var hash = [Int: Int]()
+
+    for elem in a {
+      hash[elem, default: 0] += 1
+    }
+
+    return hash
+  }
+
+  let dups = hashSet.filter({ $0.value > 1 })
+
+  return Array(dups.keys)
+}
+
+
+let a = [2, 1, 2, 1]
+let result = findDuplicates(a: a)
+print(result)
