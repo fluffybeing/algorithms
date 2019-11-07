311dcefc4e5b649358847a805645e8a00b009b12
Rahul Ranjan <rahul.rrixe@gmail.com>
2019-11-07 22:35:36 +0800

    Finish the implementation of NStacks

---
+++ Algorithms/ByteAcademy/NStacks.playground/Contents.swift
@@ -0,0 +1,43 @@
+import Cocoa
+
+class NStacks {
+  var container: [Int] = [Int]()
+  lazy var map: Dictionary<Int, [Int]> = {
+    let emptyArray = Array(repeating: [Int](), count: n)
+    return Dictionary(uniqueKeysWithValues: zip(0..<n, emptyArray))
+  }()
+
+  let n: Int
+  let capacity: Int
+  var count = 0
+
+  init(n: Int, capacity: Int) {
+    self.n = n
+    self.capacity = capacity
+  }
+
+  func push(val: Int, in stack: Int) {
+    guard stack < n, count < capacity else { return }
+
+    count += 1
+    container.append(val)
+    map[stack]?.append(container.firstIndex(of: val)!)
+  }
+
+  func pop(from stack: Int) -> Int? {
+    if let lastIndex = map[stack]?.popLast() {
+      count -= 1
+      return container[lastIndex]
+    }
+
+    return nil
+  }
+}
+
+let stack = NStacks(n: 3, capacity: 10)
+stack.push(val: 10, in: 0)
+stack.push(val: 11, in: 2)
+
+print(stack.pop(from: 0))
+print(stack.pop(from: 2))
+
