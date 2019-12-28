0c4e991b46fd13ebf7f9bc48626036c00ee3c250
Rahul Ranjan <rahul.rrixe@gmail.com>
2019-12-28 09:11:38 +0800

    Make the code correct for reverse

--- Algorithms/ByteAcademy/ReverseStack.playground/Contents.swift
+++ Algorithms/ByteAcademy/ReverseStack.playground/Contents.swift
@@ -4,7 +4,20 @@ func reverse(_ stack: inout [Int]) {
   if stack.isEmpty { return }
   let last = stack.popLast()!
   reverse(&stack)
-  stack.insert(last, at: 0)
+  
+  insertAtLast(&stack, element: last)
+}
+
+// 1, 2, 3
+func insertAtLast(_ stack: inout [Int], element: Int) {
+  if stack.isEmpty {
+    stack.append(element)
+    return
+  }
+  
+  let temp = stack.popLast()!
+  insertAtLast(&stack, element: element)
+  stack.append(temp)
 }
 
 
